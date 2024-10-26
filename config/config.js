5f638612a52b6c497d62e14ec858d79e16a4d486
yulianabreuc <abreucarrilloyv@uvm.edu.ve>
2024-10-26 00:18:28 -0400

    mejoras finales

--- config/config.js
+++ config/config.js
@@ -2,3 +2,5 @@ module.exports = {
     secretKey: 'your-secret-key',
     mongoURI: 'mongodb+srv://abreucarrilloyv:A5JQLxQKPgR3oENe@yuliana.oxcxg.mongodb.net/actividad2-1?retryWrites=true&w=majority'
 };
+
+//mongoURI en este caso esta publica pero deberia pasarse por variables de entorno para no exponerla
